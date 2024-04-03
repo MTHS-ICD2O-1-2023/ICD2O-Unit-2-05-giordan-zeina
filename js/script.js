1272a3987ea8e2e2205bd736f00895849f31b234
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-04-03 18:21:37 +0000

    deleted forms

--- js/script.js
+++ js/script.js
@@ -7,19 +7,19 @@
 "use strict"
 
 /**
- * This function calculates area of a triangle.
+ * This function calculates salary.
  */
 function calculateSalary () {
-    const TAX_RATE = 0.18
-    // input
+  const TAX_RATE = 0.18
+  // input
   const hoursWorked = parseInt(document.getElementById('hours-worked').value)
   const hourlyWage = parseInt(document.getElementById('hourly-wage').value)
-  const PAY = (hoursWorked * hourlyWage * 1.00 - 0.18)
-  const TAXES = ((hoursWorked * hourlyWage) * 0.18)
+
   // process
-  const calculateSalary = PAY + TAXES
+  const pay = (hoursWorked * hourlyWage * 1.00 - TAX_RATE)
+  const taxes = ((hoursWorked * hourlyWage) * TAX_RATE)
 
   // output
-  document.getElementById('hours-worked').innerHTML = `Your pay will be: $ ${calculateSalary.toFixed(2)}`
-  document.getElementById('hourly-wage').innerHTML = 'Your pay will be: $' + calculateSalary.toFixed(2)
+  document.getElementById('salary').innerHTML = `Your pay will be: $ ${calculateSalary.toFixed(2)}`
+  document.getElementById('salary').innerHTML = 'Your pay will be: $' + calculateSalary.toFixed(2)
 }
